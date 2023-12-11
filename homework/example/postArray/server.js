bfb8d4d7676445ab9d8c9a7184ac8d0e2515c570
Widlek <102236555+Widlek@users.noreply.github.com>
2023-12-11 15:57:47 +0300

    Delete homework directory

--- homework/example/postArray/server.js
+++
@@ -1,5 +0,0 @@
-const http = require("http");
-const router = require("./routes/index");
-
-const server = http.createServer(router);
-server.listen(1314);
--- homework/server.js
+++
@@ -1,6 +0,0 @@
-//У меня нет отдельного файла с функиями, привязанными к кнопкам, т.к. они отказывались работать. Поэтому я перенес их в index.html
-const http = require("http");
-const router = require("./router/index");
-
-const server = http.createServer(router);
-server.listen(2007);
